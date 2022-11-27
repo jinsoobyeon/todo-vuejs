58b0def1d7ff9fd3c4c49de8251032b4582d9aca
Jin Byeon <jinsoobyeon@gmail.com>
2022-11-27 23:30:39 +0900

    read and display the to-do list stored on server

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,34 @@
 <template>
-  <div>list</div>
+  <section>
+    <ul>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem['id']">
+        {{ todoItem["todo"] }}
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      todoItems: []
+    };
+  },
+  created() {
+    var xmlHttpRequest = new XMLHttpRequest();
+
+    xmlHttpRequest.open("GET", "http://localhost:8090/todo/");
+    xmlHttpRequest.onload = event => {
+      if (JSON.parse(event.target.response).length > 0) {
+        for (var i = 0; i < JSON.parse(event.target.response).length; i++) {
+          this.todoItems.push(JSON.parse(event.target.response)[i]);
+        }
+      }
+    };
+    xmlHttpRequest.send();
+  }
+};
 </script>
 
 <style></style>
